76ecf12f0e898653fdc0ac20428f994003969811
tomasz99lisowski <tomasz.lisowski99@gmail.com>
2024-12-07 17:11:24 +0100

    Custom message type added

---
+++ build/tutorial_interfaces/tutorial_interfaces__py/CMakeFiles/tutorial_interfaces__py.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "/home/tomasz/Desktop/sese_ws/build/tutorial_interfaces/rosidl_generator_py/tutorial_interfaces/_tutorial_interfaces_s.ep.rosidl_typesupport_c.c"
+  "/home/tomasz/Desktop/sese_ws/build/tutorial_interfaces/rosidl_generator_py/tutorial_interfaces/_tutorial_interfaces_s.ep.rosidl_typesupport_fastrtps_c.c"
+  "/home/tomasz/Desktop/sese_ws/build/tutorial_interfaces/rosidl_generator_py/tutorial_interfaces/_tutorial_interfaces_s.ep.rosidl_typesupport_introspection_c.c"
+  "/home/tomasz/Desktop/sese_ws/build/tutorial_interfaces/rosidl_generator_py/tutorial_interfaces/msg/__init__.py"
+  "/home/tomasz/Desktop/sese_ws/build/tutorial_interfaces/rosidl_generator_py/tutorial_interfaces/msg/_num.py"
+  "/home/tomasz/Desktop/sese_ws/build/tutorial_interfaces/rosidl_generator_py/tutorial_interfaces/msg/_num_s.c"
+  "/home/tomasz/Desktop/sese_ws/build/tutorial_interfaces/rosidl_generator_py/tutorial_interfaces/msg/_sphere.py"
+  "/home/tomasz/Desktop/sese_ws/build/tutorial_interfaces/rosidl_generator_py/tutorial_interfaces/msg/_sphere_s.c"
+  "/home/tomasz/Desktop/sese_ws/build/tutorial_interfaces/rosidl_generator_py/tutorial_interfaces/srv/__init__.py"
+  "/home/tomasz/Desktop/sese_ws/build/tutorial_interfaces/rosidl_generator_py/tutorial_interfaces/srv/_add_three_ints.py"
+  "/home/tomasz/Desktop/sese_ws/build/tutorial_interfaces/rosidl_generator_py/tutorial_interfaces/srv/_add_three_ints_s.c"
+  "CMakeFiles/tutorial_interfaces__py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/tutorial_interfaces__py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
