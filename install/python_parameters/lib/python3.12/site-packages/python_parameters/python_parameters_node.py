3aeb922334a260b8971f535305762673922f618d
tomasz99lisowski <tomasz.lisowski99@gmail.com>
2024-12-07 19:45:32 +0100

    Param tutorial added

---
+++ install/python_parameters/lib/python3.12/site-packages/python_parameters/python_parameters_node.py
@@ -0,0 +1,30 @@
+import rclpy
+import rclpy.node
+import rclpy.parameter
+
+class MinimalParam(rclpy.node.Node):
+    def __init__(self):
+        super().__init__('minimal_param_node')
+        from rcl_interfaces.msg import ParameterDescriptor
+        my_parameter_descriptor = ParameterDescriptor(description='This param is mine')
+        self.declare_parameter('my_parameter', 'world')
+        self.timer = self.create_timer(1, self.timer_callback)
+
+    def timer_callback(self):
+        my_param = self.get_parameter('my_parameter').get_parameter_value().string_value
+        self.get_logger().info('Hello %s!' % my_param)
+        my_new_param = rclpy.parameter.Parameter(
+            'my_parameter',
+            rclpy.Parameter.Type.STRING,
+            'world'
+        )
+        all_new_parameter = [my_new_param]
+        self.set_parameters(all_new_parameter)
+
+def main():
+    rclpy.init()
+    node = MinimalParam()
+    rclpy.spin(node)
+
+if __name__ == '__name__':
+    main()
