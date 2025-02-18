7739d65e4ea43098f1b86889cb034ed389a01f72
tomasz99lisowski <tomasz.lisowski99@gmail.com>
2025-02-19 00:25:21 +0100

    Marker count included

--- build/gs_package/build/lib/gs_package/gs_batt_pub.py
+++ build/gs_package/build/lib/gs_package/gs_batt_pub.py
@@ -13,6 +13,8 @@ class MinimalPublisher(Node):
         self.publisher_ = self.create_publisher(Marker, 'voltage', 10)
         self.timer = self.create_timer(0.10, self.publish_marker)  # Publish every second
         self.curr_voltage = 12.0
+        self.curr_amperage = 1.0
+        self.marker_count = 0
 
     def publish_marker(self):
         marker = Marker()
@@ -25,12 +27,12 @@ class MinimalPublisher(Node):
         marker.pose.position.x = 1.0
         marker.pose.position.y = 1.0
         marker.pose.position.z = 0.5  # Elevate slightly above the ground
-        marker.scale = Vector3(x=1.0, y=1.0, z=0.1)  # Flat square with 10cm height
-        marker.color = ColorRGBA(r=0.0, g=0.0, b=1.0, a=1.0)  # Blue color with full opacity
+        marker.scale = Vector3(x=2.0, y=2.0, z=0.2)  # Flat square with 10cm height
+        marker.color = ColorRGBA(r=0.0, g=1.0, b=0.0, a=1.0)  # Blue color with full opacity
         marker.lifetime.sec = 0  # 0 means the marker stays forever
-        marker.text = 'Battery Voltage: "%f" V' % self.curr_voltage
+        marker.text = f'Voltage: {self.curr_voltage:.2f} V\nAmperage: {self.curr_amperage:.2f} A\nCount: {self.marker_count}'
         self.curr_voltage += 0.1
-
+        self.curr_amperage += 0.05
         self.publisher_.publish(marker)
 
 
